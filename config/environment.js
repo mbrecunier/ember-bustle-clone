db69a30915246f1b160fd5426c04018732ba1e0e
chris young and Michelle Brecunier <ctopheryoung@gmail.com>
2016-03-29 14:00:49 -0700

    create individual page for each article

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'trizustle',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://trizustle.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
